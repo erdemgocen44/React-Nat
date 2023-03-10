3e10013ea012b5cc0c1c96702653fc4772905343
Erdem <erdemgocen44@gmail.com>
2023-03-10 21:50:57 +0100

    template

---
+++ 04-javascript-kategorisi/js/07String.js
@@ -0,0 +1,13 @@
+let email = "erdemgocen44@gmail.com";
+let firstName = "Erdem";
+let lastName = "GÖÇEN";
+
+console.log(email.length); //uzunlık
+
+console.log(firstName[0]); //ilk karakter
+
+console.log(firstName.charAt(0)); //ilk karakter
+
+firstName = firstName.toUpperCase();
+
+console.log(firstName);
