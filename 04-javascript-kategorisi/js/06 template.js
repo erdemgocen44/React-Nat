9f4d026885eb9c4ac4ac69e34fd8f903a9009d81
Erdem <erdemgocen44@gmail.com>
2023-03-10 21:25:34 +0100

    template

---
+++ 04-javascript-kategorisi/js/06 template.js
@@ -0,0 +1,8 @@
+let userName = "ErdemGOCEN";
+const DOMAIN = "DersimizTürkçe";
+
+let email = userName + "@" + DOMAIN;
+console.log(
+  "Merhaba ",
+  userName + " sitemize hoş geldin," + " mail adresin: " + email
+);
