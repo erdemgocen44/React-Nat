4f3db3bcb9049051fb3d73e25691bc6c4ce1a545
Erdem <erdemgocen44@gmail.com>
2023-03-10 21:32:23 +0100

    template

--- 04-javascript-kategorisi/js/06 template.js
+++ 04-javascript-kategorisi/js/06 template.js
@@ -4,5 +4,10 @@ const DOMAIN = "DersimizTürkçe";
 let email = userName + "@" + DOMAIN;
 console.log(
   "Merhaba ",
-  userName + " sitemize hoş geldin," + " mail adresin: " + email
+  userName + "sitemize hoş geldin, " + "mail adresin: " + email
 );
+
+let info = `Merhaba ${userName} sitemize hoşgeldin. mail adresin ise: ${email}
+mail adresinin uzunluğu ${email.length}
+`;
+console.log(info);
