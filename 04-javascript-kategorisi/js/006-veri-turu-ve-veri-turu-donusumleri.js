5b0e60ab261a72d498b61fb1d13ea3f4f7f2b725
Erdem <erdemgocen44@gmail.com>
2023-03-10 21:21:45 +0100

    template

--- 04-javascript-kategorisi/js/006-veri-turu-ve-veri-turu-donusumleri.js
+++ 04-javascript-kategorisi/js/006-veri-turu-ve-veri-turu-donusumleri.js
@@ -1,49 +1,40 @@
-// ********** Veri Turunu Ogrenmek ve Veri Turu Donusumleri ********** 
+// ********** Veri Turunu Ogrenmek ve Veri Turu Donusumleri **********
 
 // veri turunu ogrenmek typeof:
-let price = 111
-let stringPrice = "111"
-let hasPassword = true
+let price = 111;
+let stringPrice = "111";
+let hasPassword = true;
 
-console.log('Price: ', typeof(price),);
+console.log("Price: ", typeof price);
 
-console.log(
-    "price: ",
-    typeof(price)
-)
+console.log("price: ", typeof price);
 
-console.log(
-    "stringPrice: ",
-    typeof(stringPrice)
-)
+console.log("stringPrice: ", typeof stringPrice);
 
-console.log(
-    "hasPassword: ",
-    typeof(hasPassword)
-)
+console.log("hasPassword: ", typeof hasPassword);
 
 // string(metinsel) bilgileri int ve float'a donusturmek
-let number0= '10'
-console.log('number0: ', parseInt(number0),typeof(number0))
-number0=parseFloat(number0)
+let number0 = "10";
+console.log("number0: ", parseInt(number0), typeof number0);
+number0 = parseFloat(number0);
 
-let number1 = "11"
-number1 = parseInt(number1)
-console.log("number1: ", number1, typeof(number1) )
+let number1 = "11";
+number1 = parseInt(number1);
+console.log("number1: ", number1, typeof number1);
 
-let number2 = "11px"
-number2 = parseInt(number2)
-console.log("number2: ", number2, typeof(number2) )
+let number2 = "11px";
+number2 = parseInt(number2);
+console.log("number2: ", number2, typeof number2);
 
-let number3 = "11.1"
-number3 = Number(number3)
-console.log("number3: ", number3, typeof(number3) )
+let number3 = "11.1";
+number3 = Number(number3);
+console.log("number3: ", number3, typeof number3);
 
-let number4 = "11.4px"
-number4 = parseFloat(number4)
-console.log("number4: ", number4, typeof(number4) )
+let number4 = "11.4px";
+number4 = parseFloat(number4);
+console.log("number4: ", number4, typeof number4);
 
 // number veri tipinden string'e donusturmek:
-let number5 = 55
-number5 = number5.toString()
-console.log(number5, typeof(number5))
+let number5 = 555;
+number5 = number5.toString();
+console.log(number5, typeof number5);
