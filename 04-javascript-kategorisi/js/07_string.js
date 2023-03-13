54b5211dc97ad10cdbd273046eed6d8deefb34b0
Erdem <erdemgocen44@gmail.com>
2023-03-13 22:29:05 +0100

    String commerce

---
+++ 04-javascript-kategorisi/js/07_string.js
@@ -0,0 +1,21 @@
+let email = "erdemgocen44@gmail.com";
+let firstName = "Erdem";
+let lastName = "GÖÇEN";
+
+console.log(email.length); //uzunlık
+
+console.log(firstName[0]); //ilk karakter
+
+console.log(firstName.charAt(0)); //ilk karakter
+
+firstName = firstName.toUpperCase(); //büyük harf yapma
+
+console.log(firstName);
+
+firstName = firstName.toLowerCase(); // küçük harf yapma
+
+console.log(firstName);
+
+console.log(email.search("@")); //@ karakterini arama
+
+console.log(email[12]); //12.karakteri yazdırma
---
+++ 04-javascript-kategorisi/js/08_string.js
@@ -0,0 +1,9 @@
+let email = "erdemgocen44@gmail.com";
+let firstName = "Erdem";
+let lastName = "GÖÇEN";
+
+console.log(email.length);
+
+console.log(firstName[0]);
+
+console.log(firstName.charAt(1));
