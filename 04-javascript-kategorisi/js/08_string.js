93efe8fa5f4d62ea403b4e0ff328b9e15fc612ab
Erdem <erdemgocen44@gmail.com>
2023-03-13 22:50:24 +0100

    String commerce

--- 04-javascript-kategorisi/js/08_string.js
+++ 04-javascript-kategorisi/js/08_string.js
@@ -23,3 +23,22 @@ console.log(email[12]);
 let DOMAIN = email.slice(12);
 
 console.log(DOMAIN);
+
+console.log(DOMAIN.slice(0, DOMAIN.indexOf(".")));
+
+let fullName = `${firstName[0].toUpperCase()}${firstName
+  .slice(1)
+  .toLowerCase()} ${lastName[0].toUpperCase()}${lastName
+  .slice(1)
+  .toLowerCase()}`;
+
+console.log(fullName);
+
+//SORU
+
+let url = "www.dersimizturkce.org";
+let language = "Java";
+
+language = language.replace("Java", "JAVASCRIPT");
+
+console.log(language);
