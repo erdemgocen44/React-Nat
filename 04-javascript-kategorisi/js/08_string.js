03c15cb3376b7b07ee3f426cb16e1d081541734b
Erdem <erdemgocen44@gmail.com>
2023-03-13 22:34:57 +0100

    String commerce

--- 04-javascript-kategorisi/js/08_string.js
+++ 04-javascript-kategorisi/js/08_string.js
@@ -7,3 +7,19 @@ console.log(email.length);
 console.log(firstName[0]);
 
 console.log(firstName.charAt(1));
+
+firstName = firstName.toUpperCase();
+
+console.log(firstName);
+
+firstName = firstName.toLowerCase();
+
+console.log(firstName);
+
+console.log(email.search("@"));
+
+console.log(email[12]);
+
+let DOMAIN = email.slice(12);
+
+console.log(DOMAIN);
