99905b4e56770fb340fdbfad667ae719aa51b5ae
Erdem <erdemgocen44@gmail.com>
2024-02-22 19:28:41 +0100

    2,

--- 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
+++ 04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
@@ -1,8 +1,10 @@
 // ********** prompt ile Kullanicidan Bilgi Almak **********
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
 
-let fullName = prompt("Lutfen Adinizi Giriniz: ")
+let fullName = prompt("Lutfen Adinizi Giriniz: ");
 
-let greeting = document.querySelector("#greeting")
+console.log(fullName);
 
-greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+let greeting = document.querySelector("#greeting");
+
+greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`;
